0debf6250f0e771d8b9b1fc752a9836dccdda11e
Joshua Gustafson and Michael Schennum <mikeschenphoto@gmail.com>
2016-02-24 10:03:27 -0800

    passed second cuisine unit test

--- src/test/java/CuisineTest.java
+++ src/test/java/CuisineTest.java
@@ -12,4 +12,11 @@ public class CuisineTest {
   public void all_emptyAtFirst() {
       assertEquals(Cuisine.all().size(), 0);
   }
+
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame() {
+    Cuisine firstCuisine = new Cuisine("American");
+    Cuisine secondCuisine = new Cuisine("American");
+    assertTrue(firstCuisine.equals(secondCuisine));
+  }
 }
